20c16975befe40f938c20c5b8f310fde12516b76
lumeiqin <meiqin.lu@gisocn.com>
2022-11-14 16:24:49 +0800

    tag + step

---
+++ src/components/mt-step/mt-step.vue
@@ -0,0 +1,24 @@
+<template>
+	<view class="mt-step">
+		<view class="process_box">
+			<view class="dot"></view>
+			<view class="time">{{time}}</view>
+
+			<view class="mainBox">
+				<slot></slot>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		name: 'mt-step',
+		props: {
+			time: {
+				type: String,
+				default: "2022-10-10"
+			}
+		}
+	}
+</script>
---
+++ src/components/mt-tag/mt-tag.vue
@@ -0,0 +1,62 @@
+<template>
+	<text :style="{
+			color: selfConfig.color,
+			fontSize: selfConfig.size,
+			lineHeight: selfConfig.lineHeight,
+			border: selfConfig.border,
+			padding: selfConfig.padding,
+			background: selfConfig.bg,
+			borderRadius: selfConfig.radius,
+			transform: 'scale(' + selfConfig.scale + ')',
+			'display': 'inline-block'
+		}" @click="onClick">
+		<slot />
+	</text>
+</template>
+
+<script>
+	export default {
+		name: 'mt-tag',
+		props: {
+			config: {
+				type: Object,
+				default () {
+					return {}
+				}
+			}
+		},
+		data() {
+			return {
+				selfConfig: {
+					color: "#333",
+					size: "24rpx",
+					lineHeight: "24rpx",
+					border: "1px solid #CCC9C9",
+					padding: "6rpx 10rpx",
+					bg: "#CCC9C9",
+					radius: "20rpx",
+					scale: 1
+				}
+			}
+		},
+		watch: {
+			config(value) {
+				this.selfConfig = {
+					...this.selfConfig,
+					...value
+				}
+			}
+		},
+		created() {
+			this.selfConfig = {
+				...this.selfConfig,
+				...this.config
+			}
+		},
+		methods: {
+			onClick() {
+				this.$emit('click');
+			}
+		}
+	};
+</script>
