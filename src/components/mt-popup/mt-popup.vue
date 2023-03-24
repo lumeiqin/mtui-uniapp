df216fc5746c653c9bed6173f5f437b99e4f918a
lumeiqin <meiqin.lu@gisocn.com>
2023-03-24 09:54:00 +0800

    添加 statusChange

--- src/components/mt-popup/mt-popup.vue
+++ src/components/mt-popup/mt-popup.vue
@@ -1,77 +1,82 @@
 <template>
-	<view class="mt-popup" :class="{'mt-popup-center': type === 'center'}" v-show="popupShow"
-		@touchmove.stop.prevent="clear">
-		<view class="mt-popup-mask" :style="{
+  <view class="mt-popup" :class="{'mt-popup-center': type === 'center'}" v-show="popupShow"
+        @touchmove.stop.prevent="clear">
+    <view class="mt-popup-mask" :style="{
       background: showMask? 'rgba(0, 0, 0, 0.3)':  'rgba(0, 0, 0, 0)'
 		}" @click="maskEvent"></view>
-		<mtPopupTransition ref="mtPopupTransition" :show="transitionShow" :type="type">
-			<slot></slot>
-		</mtPopupTransition>
-	</view>
+    <mtPopupTransition ref="mtPopupTransition" :show="transitionShow" :type="type">
+      <slot></slot>
+    </mtPopupTransition>
+  </view>
 </template>
 
 <script>
-	import mtPopupTransition from "./mt-popup-transition";
-	export default {
-		name: 'mt-popup',
-		components: {
-			mtPopupTransition
-		},
-		props: {
-			show: {
-				type: [Boolean, String],
-				default: false
-			},
-			type: {
-				type: String,
-				default: 'bottom'
-			},
-      showMask: {
-				type: [Boolean, String],
-				default: true
-			},
-			isMaskClick: {
-				type: [Boolean, String],
-				default: true
-			}
-		},
-		data() {
-			return {
-				popupShow: false,
-				transitionShow: false,
-				timer: ''
-			}
-		},
-		watch: {
-			show(newvalue) {
-				this.popupShow = newvalue
-				this.transitionShow = newvalue
-			}
-		},
-		created() {
-			this.popupShow = this.show
-			this.transitionShow = this.show
-		},
-		methods: {
-			clear(e) {
-				e.stopPropagation()
-			},
-			maskEvent() {
-				if (this.isMaskClick) {
-					this.close()
-				}
-			},
-			open() {
-				this.popupShow = true
-				this.transitionShow = true
-			},
-			close() {
-				this.transitionShow = false
-				clearTimeout(this.timer)
-				this.timer = setTimeout(() => {
-					this.popupShow = false
-				}, 200)
-			}
-		}
-	}
+import mtPopupTransition from "./mt-popup-transition";
+
+export default {
+  name: 'mt-popup',
+  components: {
+    mtPopupTransition
+  },
+  props: {
+    show: {
+      type: [Boolean, String],
+      default: false
+    },
+    type: {
+      type: String,
+      default: 'bottom'
+    },
+    showMask: {
+      type: [Boolean, String],
+      default: true
+    },
+    isMaskClick: {
+      type: [Boolean, String],
+      default: true
+    }
+  },
+  data() {
+    return {
+      popupShow: false,
+      transitionShow: false,
+      timer: ''
+    }
+  },
+  watch: {
+    show(newvalue) {
+      this.popupShow = newvalue
+      this.transitionShow = newvalue
+    },
+    popupShow(newvalue) {
+      this.transitionShow = newvalue
+      this.$emit("statusChange", newvalue)
+    }
+  },
+  created() {
+    this.popupShow = this.show
+    this.transitionShow = this.show
+  },
+  methods: {
+    clear(e) {
+      e.stopPropagation()
+    },
+    maskEvent() {
+      if (this.isMaskClick) {
+        this.close()
+      }
+    },
+    open() {
+      this.popupShow = true
+      this.transitionShow = true
+    },
+    close() {
+      this.transitionShow = false
+      clearTimeout(this.timer)
+      this.timer = setTimeout(() => {
+        this.popupShow = false
+      }, 200)
+    }
+  }
+}
 </script>
--- src/pages/popup/popup.vue
+++ src/pages/popup/popup.vue
@@ -1,49 +1,55 @@
 <template>
-	<view>
-		<mt-navbar fixed title="Popup" emit="true" :config="pagenav" @backClick="backClick"></mt-navbar>
-		<view class="comp">
-			<ry-title>
-				<mt-popup ref="popup">
-					<view style="width: 100%;height: 200px;background: #fff;">
-						bottom
-					</view>
-				</mt-popup>
-				<mt-button @click="openModal('popup')">显示底部模态框</mt-button>
-			</ry-title>
+  <view>
+    <mt-navbar fixed title="Popup" emit="true" :config="pagenav" @backClick="backClick"></mt-navbar>
+    <view class="comp">
+      <ry-title>
+        <mt-popup ref="popup">
+          <view style="width: 100%;height: 200px;background: #fff;">
+            bottom
+          </view>
+        </mt-popup>
+        <mt-button @click="openModal('popup')">显示底部模态框</mt-button>
+      </ry-title>
 
 
-			<ry-title title="type: center">
-				<mt-popup ref="popup1" type="center">
-					<view style="width: 100%;height: 200px;background: #fff;">
-						center
-					</view>
-				</mt-popup>
-				<mt-button @click="openModal('popup1')">显示中间模态框</mt-button>
-			</ry-title>
+      <ry-title title="type: center">
+        <mt-popup ref="popup1" type="center" :show="popupShow" @statusChange="statusEvent">
+          <view style="width: 100%;height: 200px;background: #fff;overflow: auto">
+            center
+          </view>
+        </mt-popup>
+        <mt-button @click="openModal('popup1')">显示中间模态框</mt-button>
+      </ry-title>
 
-			<ry-title title="type: top">
-				<mt-popup ref="popup2" type="top">
-					<view style="width: 100%;height: 200px;background: #fff;">
-						center
-					</view>
-				</mt-popup>
-				<mt-button @click="openModal('popup2')">显示顶部模态框</mt-button>
-			</ry-title>
-		</view>
-	</view>
+      <ry-title title="type: top">
+        <mt-popup ref="popup2" type="top">
+          <view style="width: 100%;height: 200px;background: #fff;">
+            center
+          </view>
+        </mt-popup>
+        <mt-button @click="openModal('popup2')">显示顶部模态框</mt-button>
+      </ry-title>
+    </view>
+  </view>
 </template>
 
 <script>
-	import common from "../../common/common.js";
-	export default {
-		mixins: [common],
-		data() {
-			return {}
-		},
-		methods: {
-			openModal(type) {
-				this.$refs[type].open()
-			},
-		}
-	}
+import common from "../../common/common.js";
+
+export default {
+  mixins: [common],
+  data() {
+    return {
+      popupShow: true
+    }
+  },
+  methods: {
+    statusEvent(value) {
+      console.log(value)
+    },
+    openModal(type) {
+      this.$refs[type].open()
+    },
+  }
+}
 </script>
